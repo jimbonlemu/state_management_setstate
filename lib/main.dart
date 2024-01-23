24caf1b54384e3a071a089fbdbafd5b1f88c77c0
perompak_kode <iqbaltech29@gmail.com>
2024-01-23 22:32:41 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:state_management_setstate/done_module_list.dart';
+import 'package:state_management_setstate/module_list.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'State Management with SetState',
+      theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: ModulePage(),
+    );
+  }
+}
+
+class ModulePage extends StatefulWidget {
+  const ModulePage({super.key});
+
+  @override
+  State<ModulePage> createState() => _ModulePageState();
+}
+
+class _ModulePageState extends State<ModulePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Memulai Pemrograman Dengan Dart'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return DoneModuleList();
+                  },
+                ));
+              },
+              icon: Icon(Icons.done))
+        ],
+      ),
+      body: ModuleList(),
+    );
+  }
+}
---
+++ lib/module_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ModuleList extends StatelessWidget {
+  const ModuleList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
